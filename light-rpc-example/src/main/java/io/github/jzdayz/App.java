17eb80d330647f63af95a631c531a81256e0afff
jzdayz <1397649957@qq.com>
2020-02-17 11:11:45 +0800

    dd

--- light-rpc-example/src/main/java/io/github/jzdayz/App.java
+++ light-rpc-example/src/main/java/io/github/jzdayz/App.java
@@ -1,16 +1,18 @@
 package io.github.jzdayz;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
         Consumer bean = run.getBean(Consumer.class);
-        System.out.println(bean.get());
+        log.info("result >>>>> {}",bean.get());
         System.exit(1);
     }
 }
